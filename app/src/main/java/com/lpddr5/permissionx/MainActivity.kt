d9cfa81ba7d778059636c5280c22bb988276e219
Zhou <1696547463@qq.com>
2021-02-21 13:35:16 +0800

    完成PermissionX库的开发与测试工作。

--- app/src/main/java/com/lpddr5/permissionx/MainActivity.kt
+++ app/src/main/java/com/lpddr5/permissionx/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.lpddr5.permissionx
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.permissionx.lpddr5.PermissionX
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        makeCallBtn.setOnClickListener {
+            PermissionX.request(this,
+            Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    Log.e("Main" , true.toString())
+                    call()
+                } else {
+                    Toast.makeText(this, "你拒绝了$deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
